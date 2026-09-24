f6b26f3070327cc97e4edab7d73394518f5de7f7
Naviru Dissanayake <navirudissanayake@gmail.com>
2026-09-24 12:59:59 +0530

    Fix tsconfig reference issues

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
   output: "standalone",
   allowedDevOrigins: ["127.0.0.1", "localhost"],
   agentRules: false,
